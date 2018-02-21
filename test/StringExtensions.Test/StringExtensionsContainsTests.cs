6f7939c957dd50dee1fd374f53fcdee2347fce6e
Ben Dixon <ben.dixon@enable.com>
2018-02-21 11:29:18 +0000

    Fix tests to actually use the extension method

--- test/StringExtensions.Test/StringExtensionsContainsTests.cs
+++ test/StringExtensions.Test/StringExtensionsContainsTests.cs
@@ -1,30 +1,31 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Enable.Extensions
 {
     public class StringExtensionsContainsTests
     {
         [Theory]
-        [InlineData("foo", "fo")]
-        [InlineData("bar", "ar")]
-        [InlineData(" baz ", "baz")]
+        [InlineData("FOO", "fo")]
+        [InlineData("BaR", "ar")]
+        [InlineData(" baz ", "BAZ")]
         [InlineData(" bar ", "ba")]
         [InlineData("", "")]
         public void Contains_ReturnsTrue(string input, string subString)
         {
-            var result = input.Contains(subString);
+            var result = input.Contains(subString, StringComparison.CurrentCultureIgnoreCase);
             Assert.True(result);
         }
 
         [Theory]
         [InlineData("foo", "bar")]
-        [InlineData("bar", "foo")]
+        [InlineData("BAR", "foo")]
         [InlineData(" foo ", " bar ")]
         [InlineData("", " bar ")]
         [InlineData("foo", "fooo")]
         public void Contains_ReturnsFalse(string input, string subString)
         {
-            var result = input.Contains(subString);
+            var result = input.Contains(subString, StringComparison.CurrentCultureIgnoreCase);
             Assert.False(result);
         }
     }
