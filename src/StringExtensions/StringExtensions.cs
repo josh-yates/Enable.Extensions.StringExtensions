6f795025dc35f049ee54e6af30aa5197ded41ed7
Ben Dixon <ben.dixon@enable.com>
2018-02-21 11:13:58 +0000

    Add Contains() method and associated tests

--- src/StringExtensions/StringExtensions.cs
+++ src/StringExtensions/StringExtensions.cs
@@ -107,5 +107,18 @@ namespace Enable.Extensions
         {
             return s.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
         }
+
+        /// <summary>
+        /// Determines whether the specified string contains the specified value
+        /// </summary>
+        public static bool Contains(this string s, string value, StringComparison comparisonType)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException(nameof(s));
+            }
+
+            return s.IndexOf(value, comparisonType) > -1;
+        }
     }
 }
---
+++ test/StringExtensions.Test/StringExtensionsContainsTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace Enable.Extensions
+{
+    public class StringExtensionsContainsTests
+    {
+        [Theory]
+        [InlineData("foo", "fo")]
+        [InlineData("bar", "ar")]
+        [InlineData(" baz ", "baz")]
+        [InlineData(" bar ", "ba")]
+        [InlineData("", "")]
+        public void Contains_ReturnsTrue(string input, string subString)
+        {
+            var result = input.Contains(subString);
+            Assert.True(result);
+        }
+
+        [Theory]
+        [InlineData("foo", "bar")]
+        [InlineData("bar", "foo")]
+        [InlineData(" foo ", " bar ")]
+        [InlineData("", " bar ")]
+        [InlineData("foo", "fooo")]
+        public void Contains_ReturnsFalse(string input, string subString)
+        {
+            var result = input.Contains(subString);
+            Assert.False(result);
+        }
+    }
+}
